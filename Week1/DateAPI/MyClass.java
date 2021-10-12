a22e1503f4497db0658b51112bd247399dd5390a
Akash Kumar Tanwar <tanwarakash69@gmail.com>
2021-10-12 18:34:10 +0530

    Date API program comments added

--- Week1/DateAPI/MyClass.java
+++ Week1/DateAPI/MyClass.java
@@ -4,11 +4,15 @@ import java.util.Date;
 public class MyClass {
     public static void main(String[] args){
 
+        //creating date object
         Date date = new Date();
         System.out.println(date);
 
+        // currentTimeMillis() method returns milli seconds since 1 JAN , 1970
         long milli = System.currentTimeMillis();
-        Date d = new Date(milli);
+
+        Date d = new Date(milli); 
         System.out.println(d);
+        d.
     }
 }
