865aac83835dc0b88c54ecb41c835181032fa60c
Akash Kumar Tanwar <tanwarakash69@gmail.com>
2021-10-14 17:56:51 +0530

    comments removed from default constructor program

--- Week2/constructor/DefaultConst.java
+++ Week2/constructor/DefaultConst.java
@@ -1,7 +1,5 @@
 package Week2.constructor;
 
-import org.graalvm.compiler.lir.LIRInstruction.Def;
-
 public class DefaultConst {
     
     String name;
@@ -9,7 +7,6 @@ public class DefaultConst {
 
     public static void main(String[] args){
         DefaultConst emp1 = new DefaultConst();
-        /* using new keyword emp1 object is created and using default constructor call its attributes are initialized to their default values*/
 
         System.out.println("emp1 name is: "+emp1.name);
         System.out.println("emp1 id is: "+emp1.empId);
