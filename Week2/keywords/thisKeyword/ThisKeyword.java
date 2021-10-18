c74c0897f45be80200dad416e0e5eb115a915fca
Akash Kumar Tanwar <tanwarakash69@gmail.com>
2021-10-18 17:07:31 +0530

    This keyword first program is added

---
+++ Week2/keywords/thisKeyword/ThisKeyword.java
@@ -0,0 +1,21 @@
+package Week2.keywords.thisKeyword;
+
+public class ThisKeyword {
+    String name;
+    int id;
+
+    void setValues(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    void showDetails(){
+        System.out.println("Name is: "+name);
+        System.out.println("Id is: "+id);
+    }
+    public static void main(String[] args){
+        ThisKeyword obj1 = new ThisKeyword();
+        obj1.setValues("Akash", 101);
+        obj1.showDetails();
+    }
+}
