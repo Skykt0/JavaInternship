f75a1a5ca145e215c6621a942f08a43b3341dcda
Akash Kumar Tanwar <tanwarakash69@gmail.com>
2021-10-18 17:23:52 +0530

    Invoke constructor using this() method

---
+++ Week2/keywords/thisKeyword/InvokeConstructor.java
@@ -0,0 +1,23 @@
+package Week2.keywords.thisKeyword;
+
+public class InvokeConstructor {
+
+    String name; 
+    int id;
+    public static void main(String args[]){
+        InvokeConstructor obj1 = new InvokeConstructor("Akash",101);
+    }
+
+    InvokeConstructor(){
+        System.out.println("message from no argument constructor");
+    }
+
+    InvokeConstructor(String name, int id){
+        this();
+        this.name = name;
+        this.id = id;
+        System.out.println("output from parameterized constructor");
+        System.out.println("Name is: "+this.name);
+        System.out.println("Id is: "+this.id);
+    }
+}
