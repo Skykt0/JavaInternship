b8602189350019992dc4666ad23ca226cb2215dd
Akash Kumar Tanwar <tanwarakash69@gmail.com>
2021-10-19 10:34:02 +0530

    protected modifier program is added

---
+++ Week2/accessmodifiers/protectedmodifier/protectedmethod/ProtectedMethod.java
@@ -0,0 +1,17 @@
+package Week2.accessmodifiers.protectedmodifier.protectedmethod;
+
+public class ProtectedMethod 
+{   
+    String name;
+    int id;
+    protected void setValues(String name, int id)
+    {
+        this.name = name; 
+        this.id = id;
+    }
+    protected void display()
+    {
+        System.out.println("Name is: "+this.name);
+        System.out.println("Id is: "+this.id);
+    }
+}
