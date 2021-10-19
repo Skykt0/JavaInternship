4c76e7cd3c263176a6df1535a6841a6920391dbb
Akash Kumar Tanwar <tanwarakash69@gmail.com>
2021-10-19 10:20:11 +0530

    default package program is added

---
+++ Week2/accessmodifiers/defaultmodifier/defaultpackage/DefaultClass.java
@@ -0,0 +1,8 @@
+package Week2.accessmodifiers.defaultmodifier.defaultpackage;
+
+class DefaultClass {
+    void message()
+    {
+        System.out.println("this message is from Default package class");
+    }
+}
