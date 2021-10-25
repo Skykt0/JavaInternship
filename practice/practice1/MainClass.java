9ce7c92c2377fa3dbb5eb4a4bac47280b1146229
Akash Kumar Tanwar <70046812+Skykt0@users.noreply.github.com>
2021-10-25 13:10:08 +0530

    Update MainClass.java
    Modified the code

--- practice/practice1/MainClass.java
+++ practice/practice1/MainClass.java
@@ -108,13 +108,13 @@ public class MainClass
            {
                System.out.println();
                System.out.println("Not Sucessful: ");
-               System.exit(0);   
+
            }
        }
        else{
            System.out.println();
            System.out.println("Not Sucessful: ");
-           System.exit(0);
+           
        }
        
    }
