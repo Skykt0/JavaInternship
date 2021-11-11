113cf1cdb0a03e8b9cc44f8e2c3a448f8bfcf097
Akash Kumar Tanwar <tanwarakash69@gmail.com>
2021-11-11 16:51:13 +0530

    throw keyword example code added

---
+++ Week4/exceptionhandling/ThrowKeyword.java
@@ -0,0 +1,29 @@
+package Week4.exceptionhandling;
+
+import java.util.Scanner;
+
+public class ThrowKeyword {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        
+        try {
+            validate(age);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("Program ended successfully..");
+    }
+    static void validate(int age)
+    {
+        if(age<18){
+            throw new ArithmeticException("Not eligible for vote");
+        }
+        else{
+            System.out.println("eligible for vote");
+        }
+    }
+}
