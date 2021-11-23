61284bd04c64fc16ae471734557a7dcf60a57671
Akash Kumar Tanwar <tanwarakash69@gmail.com>
2021-11-23 14:07:11 +0530

    interface example modified

--- Week3/interfacetest/InterfaceTest.java
+++ Week3/interfacetest/InterfaceTest.java
@@ -1,36 +1,66 @@
 package Week3.interfacetest;
 
-public class InterfaceTest {
-    public static void main(String args[]){
-        B b = new B();
-        b.profession();
-        b.age();
-
-        A a = new A();
-        a.profession();
-        a.age();
-    }
-}
-interface Person{
-    void profession();
-    void age();
-}
-
-class A implements Person{
-    public void profession(){
-        System.out.println("A is a doctor.");
-    }
-
-    public void age(){
-        System.out.println("A's age is 33");
+public class InterfaceTest 
+{
+    public static void main(String args[])
+    {
+        Tesla tesla = new Tesla();
+        System.out.println("Tesla car: ");
+        tesla.speed();
+        tesla.runsOn();
+        tesla.turnOnAutoPilot();
+        System.out.println();
+        
+        BMW bmw = new BMW();
+        System.out.println("BMW car: ");
+        bmw.speed();
+        bmw.runsOn();
+        bmw.turnOnDrivingAssistent();
     }
 }
 
-class B implements Person{
-    public void profession(){
-        System.out.println("B is an IAS officer");
+interface Car
+{
+    int noOfWhells = 4; 
+    void speed();
+    void runsOn();    
+}
+
+class Tesla implements Car
+{
+    public void speed()
+    {
+        System.out.println("tesla can go from 0 to 60mph in 1.99 seconds..");
+        System.out.println("tesla has top speed of 200mph..");
     }
-    public void age(){
-        System.out.println("B's age is 27");
+
+    public void runsOn()
+    {
+        System.out.println("tesla cars runs on electricity.");
+    }
+
+    public void turnOnAutoPilot()
+    {
+        System.out.println("your car will drive automatically now...");
+    }
+
+}
+
+class BMW implements Car
+{
+    public void speed()
+    {
+        System.out.println("bwm can go from 0 to 60mph in 3.1 seconds..");
+        System.out.println("bmw has top speed of 250mph..");
+    }
+    public void runsOn()
+    {
+        System.out.println("BMW cars runs on petrol and diesel.");
+    }
+    public void turnOnDrivingAssistent()
+    {
+        System.out.println("your car will be parked automatically..");
     }
 }
+
+
